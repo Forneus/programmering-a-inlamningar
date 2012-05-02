8110c9a06b4cee877cdba84830fe7db83b7a9f9f
Marcus <mackan_heavymetal@hotmail.com>
2012-05-02 10:53:01 +0200

    Har ar borjan till mitt inlamningsarbete. Antligen fatt lite fart pa att mala strecken, och borjar bli nojd med grunden. Undrar daremot over varfor den sista alltid blir i mer graaktig farg

---
+++ noter.js
@@ -0,0 +1,60 @@
+"use strict"
+var draw = startCanvas("maincanvas");
+
+draw.raw().moveTo(700, 100);
+draw.raw().lineTo(100, 100);
+draw.raw().stroke();
+
+draw.raw().moveTo(700, 125);
+draw.raw().lineTo(100, 125);
+draw.raw().stroke();
+
+draw.raw().moveTo(700, 150);
+draw.raw().lineTo(100, 150);
+draw.raw().stroke();
+
+draw.raw().moveTo(700, 175);
+draw.raw().lineTo(100, 175);
+draw.raw().stroke();
+
+draw.raw().moveTo(700, 200);
+draw.raw().lineTo(100, 200);
+draw.raw().stroke();
+
+
+
+
+
+
+
+draw.raw().moveTo(700, 300);
+draw.raw().lineTo(100, 300);
+draw.raw().stroke();
+
+draw.raw().moveTo(700, 325);
+draw.raw().lineTo(100, 325);
+draw.raw().stroke();
+
+draw.raw().moveTo(700, 350);
+draw.raw().lineTo(100, 350);
+draw.raw().stroke();
+
+draw.raw().moveTo(700, 375);
+draw.raw().lineTo(100, 375);
+draw.raw().stroke();
+
+draw.raw().moveTo(700, 400);
+draw.raw().lineTo(100, 400);
+draw.raw().stroke();
+
+// beginPath?
+
+
+
+
+
+
+
+
+// TODO:Remember to check for unicode-symbols, or consider painting them yourself.
+// quarternote: hexadecimal: u2669      G clef: (U+1D11E)
