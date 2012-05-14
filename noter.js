4e27fa852b4ea5da79db5e93774e85f6cc71a86f
Lars Gunther <gunther@keryx.se>
2012-05-15 00:00:34 +0200

    Noter visas - linjerna svarta pga bredd 2

--- noter.js
+++ noter.js
@@ -7,8 +7,9 @@ function notstreck(ypos) {
     // draw.raw().stroke();
 };
 
-draw.setCurColor("black");
+// draw.setCurColor("black");
 draw.raw().beginPath();
+draw.raw().lineWidth = 2;
 for (var i=100; i < 201; i += 25) {
     notstreck(i);
 }
@@ -16,9 +17,10 @@ for (var i=300; i < 401; i += 25) {
     notstreck(i);
 }
 // Utan denna extra stroke blir linjen grå och inte svart - knepigt!
-draw.raw().closePath();
+//draw.raw().closePath();
 draw.raw().stroke();
-draw.raw().stroke();
+// draw.raw().stroke();
+
 // fill color också?
 /*
 var lines = [];
@@ -57,11 +59,24 @@ draw.raw().stroke();
 
 
 // beginPath?
+// \u1D15D = full
+// \u1D15E = half
+// \u1D15F = quarter
+// \u1D160 = eigth
 
-// \u2669
+// Eller
 
-draw.text("\u2669", 100, 200, "#000", 50);
+// \u2669 = 1/4
+// \u266A = 1/8
 
+var set = 209, space = 60
+for ( i = 0; i < 9; i += 1 ) {
+    draw.text("\u2669", i*space + 100, set - 12.5 * i, "#000", 130);
+}
+set = 409;
+for ( i = 0; i < 9; i += 1 ) {
+    draw.text("\u266A", i*space + 100, set - 12.5 * i, "#000", 130);
+}
 
 
 
