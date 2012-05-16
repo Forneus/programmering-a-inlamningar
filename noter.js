25adf18c95dc84928e826b19c5a78ad7c2fd04a1
Marcus <mackan_heavymetal@hotmail.com>
2012-05-16 11:12:13 +0200

    Borjar leta tangetfunktion

--- noter.js
+++ noter.js
@@ -9,6 +9,7 @@ function notstreck(ypos) {
 
 draw.setCurColor("black");
 draw.raw().beginPath();
+draw.raw().lineWidth = 2;
 for (var i=100; i < 201; i += 25) {
     notstreck(i);
 }
@@ -18,7 +19,7 @@ for (var i=300; i < 401; i += 25) {
 // Utan denna extra stroke blir linjen grå och inte svart - knepigt!
 draw.raw().closePath();
 draw.raw().stroke();
-draw.raw().stroke();
+
 // fill color också?
 /*
 var lines = [];
@@ -60,10 +61,55 @@ draw.raw().stroke();
 
 // \u2669
 
-draw.text("\u2669", 100, 200, "#000", 50);
+draw.text("\u1D11E", 150, 100, "#000", 80);
+
+draw.text("\u2669", 200, 195, "#000", 80);
+draw.text("\u2669", 300, 170, "#000", 80);
+
+
+// object.onkeypress=
+
+
+
+// Försökt hitta lösningar på att avgöra vilken knapp som trycks för att kunna utföra en funktion, men hittils inte hittat någon lösning. Koden som följer är test på ifall det funkar.
+
+
+/*
+function drawnote(e){
+var evtobj=window.event? event : e //distinguish between IE's explicit event object (window.event) and Firefox's implicit.
+var unicode=evtobj.charCode? evtobj.charCode : evtobj.keyCode
+var actualkey=String.fromCharCode(unicode)
+if (actualkey=="a")
+draw.text("\u2669", 300, 170, "#000", 80);
+if (actualkey=="z")
+document.body.style.fontSize="100%"
+}
+document.onkeypress=drawnote
+
+/*
 
 
 
 
+
+// Här är en hittils irrelevant del av programmet, som jag jobbar med när jag kommer till steget att manuellt kunna välja vad för slags not som skall skrivas ut.
+
+/*  document.getElementById("Helnot").onclick = function () {
+      draw.text("\u2669", 400, 170, "#000", 80);
+}
+document.getElementById("Halvnot").onclick = function () {
+    draw.text("\u2669", 400, 170, "#000", 80);
+}
+document.getElementById("Fjärdedelsnot").onclick = function () {
+    draw.text("\u2669", 400, 170, "#000", 80);
+}
+document.getElementById("Åttondelsnot").onclick = function () {
+    draw.text("\u2669", 400, 170, "#000", 80);
+}
+*/
+
+
+
+// TODO NOTE: +/- 25 pixlar för noter, precis som notstrecken
 // TODO:Remember to check for unicode-symbols
 // quarternote: hexadecimal: u2669      G clef: (U+1D11E)
