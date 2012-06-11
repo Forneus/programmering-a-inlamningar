2712e7f39e1eef57b5146212499dba8da6fba6d4
Marcus <mackan_heavymetal@hotmail.com>
2012-06-11 13:30:41 +0200

    wip

--- noter.js
+++ noter.js
@@ -4,7 +4,6 @@ var draw = startCanvas("maincanvas");
 function notstreck(ypos) {
     draw.raw().moveTo(700, ypos);
     draw.raw().lineTo(100, ypos);
-    // draw.raw().stroke();
 };
 
 draw.setCurColor("black");
@@ -16,11 +15,10 @@ for (var i=100; i < 201; i += 25) {
 for (var i=300; i < 401; i += 25) {
     notstreck(i);
 }
-// Utan denna extra stroke blir linjen grå och inte svart - knepigt!
+
 draw.raw().closePath();
 draw.raw().stroke();
 
-// fill color också?
 
 function fixedFromCharCode(codePt) {
     if (codePt > 0xFFFF) {
@@ -38,17 +36,6 @@ var quartnote = fixedFromCharCode(0x1D15F);
 var eigthnote = fixedFromCharCode(0x1D160);
 var gclef     = fixedFromCharCode(0x1D11E);
 
-// Försökt hitta lösningar på att avgöra vilken knapp som trycks för att kunna utföra en funktion, men hittils inte hittat någon lösning. Koden som följer är test på ifall det funkar.
-
-
-
-/* function drawnote(e){
-var unicode = e.keyCode;
-var actualkey=String.fromCharCode(unicode)
-if (actualkey=="a")
-draw.text("\u2669", 300, 170, "#000", 80);
-if (actualkey=="z")
-} */
 
 //document.body.style.fontSize="100%"
 
@@ -63,6 +50,8 @@ draw.setCurFont("Code2001");
 var set = 202, space = 58, startleft = 175, fontsize = 130;
 draw.text(gclef, 100, set-5, "#000", fontsize);
 draw.text(gclef, 100, set+195, "#000", fontsize);
+
+
 /*
 for ( i = 0; i < 9; i += 1 ) {
     draw.text(halfnote, i*space + startleft, set - 12.5 * i, "#000", fontsize);
@@ -78,7 +67,6 @@ for ( i = 0; i < 9; i += 1 ) {
 */  
 
 
-// Här är en hittils irrelevant del av programmet, som jag jobbar med när jag kommer till steget att manuellt kunna välja vad för slags not som skall skrivas ut.
 
 var noty = 165;
 var plats = 0;
@@ -88,7 +76,7 @@ document.getElementById("Helnot").onclick = function () {
         noty = noty + 200;
         plats = 0;
     }
-    draw.circle(startleft+space*plats, noty, 10, "#000", true);
+    draw.circle(startleft+space*plats, noty-3, 11, "#000", true);
     plats +=1;
 }
 
